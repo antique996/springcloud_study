58631c1fa5812dca1228fcfb5abc6edee01f4747
antique <1641840239@qq.com>
2020-12-10 22:36:36 +0800

    配置eureka集群

--- springcloud-eureka-7002/src/main/java/com/gu/springcloud/EurekaServer_7002.java
+++ springcloud-eureka-7002/src/main/java/com/gu/springcloud/EurekaServer_7002.java
@@ -2,12 +2,12 @@ package com.gu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptConsumer_80 {
+@EnableEurekaServer
+public class EurekaServer_7002 {
     public static void main(String[] args) {
-        SpringApplication.run(DeptConsumer_80.class, args);
+        SpringApplication.run(EurekaServer_7002.class, args);
     }
 }
